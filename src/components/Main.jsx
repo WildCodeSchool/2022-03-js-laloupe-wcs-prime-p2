a7ecf6a614da61f7f9db0c8ed355f1b103f032df
ElixirMeow <elixirmeow@gmail.com>
2022-04-13 17:25:49 +0200

    working on icons/footer links

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import Home from "@pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Categories from "@pages/Categories";
+import CoupDeCoeur from "@pages/CoupDeCoeur";
+import Films from "@pages/Films";
+
+const Main = () => {
+  return (
+    <main className="mainpage">
+      <section>blablalblalbab</section>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Categories" element={<Categories />} />
+        <Route path="/CoupDeCoeur" element={<CoupDeCoeur />} />
+        <Route path="/Films" element={<Films />} />
+      </Routes>
+    </main>
+  );
+};
+export default Main;
