41c635469bd31d9f9cc910b936e219aced43a8f6
ElixirMeow <elixirmeow@gmail.com>
2022-04-13 11:44:56 +0200

    Added routes / Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Categories">Categories</Link>
+        <Link to="/CoupDeCoeur">Coup de Coeur</Link>
+        <Link to="/Films">Films</Link>
+      </nav>
+    </footer>
+  );
+}
