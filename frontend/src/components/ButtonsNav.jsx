b9c3bc5a08d751d76290a62209cadd6c02023ad8
ElixirMeow <elixirmeow@gmail.com>
2022-04-13 17:25:49 +0200

    working on icons/footer links

---
+++ frontend/src/components/ButtonsNav.jsx
@@ -0,0 +1,13 @@
+import "./buttonsnav.scss";
+
+export default function ButtonNav() {
+  return (
+    <div>
+      <ul className="ButtonsNav">
+        <li>Home</li>
+        <li>Cat</li>
+        <li>CDC</li>
+      </ul>
+    </div>
+  );
+}
