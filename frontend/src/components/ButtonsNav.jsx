a36eec81087e3db2cc53015bd120133cf698b080
ElixirMeow <elixirmeow@gmail.com>
2022-04-14 14:35:50 +0200

    Finished Footer and functionnal routing

--- frontend/src/components/ButtonsNav.jsx
+++ frontend/src/components/ButtonsNav.jsx
@@ -1,12 +1,31 @@
 import "./buttonsnav.scss";
-
+import { Link } from "react-router-dom";
+import HomeIcons from "../assets/Home.png";
+import CatIcons from "../assets/Categories.png";
+import CoeurIcons from "../assets/Coeur.png";
+import Home from "@pages/Home";
 export default function ButtonNav() {
   return (
     <div>
       <ul className="ButtonsNav">
-        <li>Home</li>
-        <li>Cat</li>
-        <li>CDC</li>
+        <Link to="/">
+          <li>
+            <img className="imgFooter" src={HomeIcons} alt="Home Icons" />
+            <p>Home</p>
+          </li>
+        </Link>
+        <Link to="/Categories">
+          <li>
+            <img className="imgFooter" src={CatIcons} alt="Categories Icons" />
+            <p>Genres</p>
+          </li>
+        </Link>
+        <Link to="/CoupDeCoeur">
+          <li>
+            <img className="imgFooter" src={CoeurIcons} alt="Coup de coeur" />
+            <p>Favorite</p>
+          </li>
+        </Link>
       </ul>
     </div>
   );
