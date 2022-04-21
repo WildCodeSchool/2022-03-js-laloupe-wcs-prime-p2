ebf388680cf905e5d8da8a9c9b89d4e757ea3a21
anislrg <anislrg.perso@gmail.com>
2022-04-21 10:15:37 +0200

    carousels des nouvautées terminé et lier à l'API

--- src/components/Carousels.jsx
+++ src/components/Carousels.jsx
@@ -1,24 +1,68 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import tab from "@components/tableau";
+
 import "./Carousels.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Carousels = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 3,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          autoplay: true,
+          autoplaySpeed: 4000,
+          pauseOnHover: true,
+        },
+      },
+    ],
   };
+  const [image, setImage] = useState([]);
+  const getImage = () => {
+    // Send the request
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/upcoming?api_key=94b0467408dc50f0db83378d241839d5"
+      )
+      // Extract the DATA from the received response
+      .then((response) => {
+        return response.data;
+      })
+      // Use this data to update the state
+      .then((data) => {
+        setImage(data.results);
+      });
+  };
+
+  useEffect(() => {
+    getImage();
+  }, []);
   return (
     <Slider {...settings}>
-      {tab.map((item) => (
+      {image.map((item) => (
         <div key={`carousel-${item.id}`} className="carousels">
           <div className="card">
             <div className="card-top">
-              <img src={item.image} alt="" />
+              <img
+                src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
+                alt=""
+              />
+              <h2>{item.original_title}</h2>
             </div>
           </div>
         </div>
