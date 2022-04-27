3b0a7165a42d3f73e8dfc993b14c4510334d08a4
ElixirMeow <elixirmeow@gmail.com>
2022-04-27 14:12:44 +0200

    Working on movieDetails

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -2,7 +2,7 @@ import Home from "@pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Categories from "@pages/Categories";
 import CoupDeCoeur from "@pages/CoupDeCoeur";
-import Films from "@pages/Films";
+import Films from "./Films";
 import DisplayMovies from "./DisplayMovies";
 
 const Main = () => {
@@ -12,8 +12,8 @@ const Main = () => {
         <Route path="/" element={<Home />} />
         <Route path="/Categories" element={<Categories />} />
         <Route path="/Categories/:genre" element={<DisplayMovies />} />
-        <Route path="/CoupDeCoeur" element={<CoupDeCoeur />} />
         <Route path="/Films" element={<Films />} />
+        <Route path="/CoupDeCoeur" element={<CoupDeCoeur />} />
       </Routes>
     </main>
   );
