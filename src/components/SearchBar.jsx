cba1b6dc918453f62f58686a93ab643e27b03be5
coulange severine <coulsev@gmail.com>
2022-04-27 09:26:40 +0200

    barre de recherche

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,48 @@
 import React from "react";
-import "./SearchBar.scss";
+
 import { useState, useEffect } from "react";
-import axios from "axios";
+
 
 
 const SearchBar= ()=> {
 
-  const [moviesData, setMoviesData] = useState([]);
-  const [search, setSearch] = useState([]),
-  
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=37c6f95d37566ae6e16181a80c84a1e9&query=${search}`
-      )
-      .then((res) => setMoviesData(res.data.results));
-  }, [search]);
+  const [datas, setDatas]= useState([]);
+  const [searchTerm, setSearchTerm]= useState("");
 
- 
-  return (
-    <div className="Searchbar">
-    <input
-            type="text"
-            placeholder="Search Movie"
-            id="search-input"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <input type="submit" value="Search" />
+  useEffect(()=> {
+    fetch('https://api.themoviedb.org/3/movie/550?api_key=37c6f95d37566ae6e16181a80c84a1e9')
+    .then(response=> response.json())
+    .then(json=> setDatas(json))
+}, []);
+
+const handleSearchTerm = (e) => {
+let value =e.target.value;
+setSearchTerm(value);
+
+
+};
   
+return ( 
+  <>
+    <div className="search">
+      <input type="text" name="searchBar" id="searchBar" placeholder="search" onChange={handleSearchTerm}/>
     </div>
+    <div className="search_results">
+      {datas.filter((val) => {
+        return val.title.toLowerCase().includes(searchTerm.toLowerCase());
+      })
+      .map((val) => {
+      return (
+      <div className="search_result" key={val.id}>{val.title}</div>
+      );
+      })}
+    </div>
+  </>
   );
 }
+  
+  
+  
+  
 
 export default SearchBar;
