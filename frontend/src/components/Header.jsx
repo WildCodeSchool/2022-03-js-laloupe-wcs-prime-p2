088cf8c2e82582208de0d9448b9042e462a3183d
coulange severine <coulsev@gmail.com>
2022-04-27 09:26:40 +0200

    barre de recherche

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import "./Header.css";
-
-
+import "./Header.css"
+import SearchBar from "./SearchBar";
 
 const Header = () => {
   return (
     <header>
       <div className="logo">
         <img src={logo} alt="logo" />
+        </div>
+        <div className="searchBar">
+          <SearchBar/>
         </div>
       </header>
   );
