df1dfdb64c91d54f0646eb95861f88378e411aee
ElixirMeow <elixirmeow@gmail.com>
2022-04-28 17:21:58 +0200

    genres movie display done

--- frontend/src/components/Films.jsx
+++ frontend/src/components/Films.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// // import { useParams } from "react-router-dom";
+// import axios from "axios";
 
-const filmsEx = () => {
-  // const { films } = parseInt(useParams().id, 10);
-  const [filmsDetails, setFilmsDetails] = useState();
+// const filmsEx = () => {
+//   // const { films } = parseInt(useParams().id, 10);
+//   const [filmsDetails, setFilmsDetails] = useState();
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/discover/movie/?api_key=442e533a3f611fdf2ce32b99e46a8d6b"
-      )
-      .then(({ data }) => data)
-      .then((data) => setFilmsDetails(data.results));
-  }, []);
-  console.warn(filmsDetails);
-  return <p>HEH</p>;
-};
+//   useEffect(() => {
+//     axios
+//       .get(
+//         "https://api.themoviedb.org/3/discover/movie/?api_key=442e533a3f611fdf2ce32b99e46a8d6b"
+//       )
+//       .then(({ data }) => data)
+//       .then((data) => setFilmsDetails(data.results));
+//   }, []);
+//   console.warn(filmsDetails);
+//   return <p>HEH</p>;
+// };
 
-export default filmsEx;
+// export default filmsEx;
