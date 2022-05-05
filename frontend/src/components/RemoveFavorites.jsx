20fac4aebb6988adcfec146c0d7abbdad0119728
coulange severine <coulsev@gmail.com>
2022-05-05 11:21:13 +0200

    searchBar

--- frontend/src/components/RemoveFavorites.jsx
+++ frontend/src/components/RemoveFavorites.jsx
@@ -1,12 +1,11 @@
-import react from "react"
+import react from "react";
 
 const RemoveFavourites = () => {
-	return (
-		<>
-			<span className='Remove favourites'>Remove from favourites</span>
-			
-		</>
-	);
+  return (
+    <>
+      <span className="Remove favourites">Remove from favourites</span>
+    </>
+  );
 };
 
 export default RemoveFavourites;
--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
+import "./SearchBox.scss";
 
 const searchBox = (props) => {
-    return (
-        <div>
-           <div className="search">
-           <input
-				className='form-control'
-				value={props.value}
-				onChange={(event) => props.setSearchValue(event.target.value)}
-				placeholder='Type to search...'
-			></input>
-            </div>
-        </div>
-    );
+  return (
+    <div className="search">
+      <input
+        className="form-control"
+        value={props.value}
+        onChange={(event) => props.setSearchValue(event.target.value)}
+        placeholder="Type to search..."
+      ></input>
+    </div>
+  );
 };
 
 export default searchBox;
