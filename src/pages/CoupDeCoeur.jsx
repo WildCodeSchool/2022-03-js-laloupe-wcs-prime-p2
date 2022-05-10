587e2f673b4ddf5bfebd8c63e09f40564a13317b
ElixirMeow <elixirmeow@gmail.com>
2022-05-10 20:10:14 +0200

    meh

--- src/pages/CoupDeCoeur.jsx
+++ src/pages/CoupDeCoeur.jsx
@@ -10,7 +10,7 @@ const UserList = () => {
     : [];
 
   useEffect(() => {
-    for (let i = 0; i < moviesId.length; i++) {
+    for (let i = 0; i < moviesId.length; i) {
       axios
         .get(
           `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=94b0467408dc50f0db83378d241839d5`
