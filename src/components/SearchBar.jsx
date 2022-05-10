246acbb603d42c07345f343a39f31cc6692f1423
Xavier Georget <xageorget@gmail.com>
2022-05-10 09:39:22 +0200

    mod url for axios request in SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,7 +9,7 @@ const SearchBar = ({ setMovies }) => {
   // const [favourites, setFavourites] = useState([]);
 
   const getMovieRequest = async () => {
-    const url = `https:api.themoviedb.org/3/search/movie?api_key=37c6f95d37566ae6e16181a80c84a1e9&language=en-US&query=${searchValue}`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=37c6f95d37566ae6e16181a80c84a1e9&language=en-US&query=${searchValue}`;
 
     const response = await axios.get(url);
     const { data } = response;
