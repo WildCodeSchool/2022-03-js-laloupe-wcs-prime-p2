e7d7b377c8e7daa90c84d47c77cf90bdd7232222
ElixirMeow <elixirmeow@gmail.com>
2022-05-11 13:54:57 +0200

    done

--- frontend/src/components/MovieList.jsx
+++ frontend/src/components/MovieList.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable */
+import DisplayComponent from "./DisplayComponent";
 import "./MovieList.scss";
+import { useState } from "react";
+import Modal from "./Modal";
+import useModal from "./UseModal";
 
 const MovieList = ({ movies, FavouriteComponent }) => {
+  const { isShowing, toggle } = useModal();
   return (
     <div>
       {movies.map((movie) => (
         <div key={movie.id} className="image-container">
-          {movie.poster_path && (
+          <DisplayComponent
+            details
+            key={movie.id}
+            id={movie.id}
+            name={movie.name}
+            title={movie.title}
+            overview={movie.overview}
+            image={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            vote={movie.vote_average}
+            date={movie.release_date}
+          />
+          {/* {movie.poster_path && (
             <img
+              key={movie.id}
               src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
               alt="movie"
             />
-          )}
+          )} */}
           <div
             role="button"
             tabIndex="0"
